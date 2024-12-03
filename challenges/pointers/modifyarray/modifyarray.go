82d5ad8544bd0de1ccaf2e6df5a6f5f71781c0ea
Julian Lobbes <julian@lobbes.dev>
2024-12-03 21:58:57 +0100

    add solutions to `numberswap` and `modifyarray`

---
+++ challenges/pointers/modifyarray/modifyarray.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    arr := []int{1, 2, 3, 4, 5}
+    fmt.Println("Original Array:", arr)
+    DoubleArray(&arr)
+    fmt.Println("Modified Array:", arr)
+}
+
+func DoubleArray(arr *[]int) {
+    const l = 5
+    a := *arr
+    for i := 0; i < l; i++ {
+        a[i] *= 2
+    }
+}
---
+++ challenges/pointers/numberswap/numberswap.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    a, b := 5, 10
+    fmt.Println("Before Swap: a =", a, "b =", b)
+    Swap(&a, &b)
+    fmt.Println("After Swap: a =", a, "b =", b)
+}
+
+func Swap(a, b *int) {
+    x := *a
+    *a = *b
+    *b = x
+}
